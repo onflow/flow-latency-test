fd44bee9d08b5e7527dd50cdc49ebc5c6e16b94a
Tang Bo Hao <tech@btang.cn>
2025-03-20 23:56:36 +0800

    chore: enable import organization in biome.json and format code in utils for consistency

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,104 +6,104 @@ import type { Context, EVMBlockchainContext } from "./types";
 export * from "./config";
 
 export function logTimeWrapper(fn: (...args: unknown[]) => Promise<unknown>) {
-	return async (...args: unknown[]) => {
-		console.time(`Function Call [${fn.name}]`);
-		const result = await fn(...args);
-		console.timeEnd(`Function Call [${fn.name}]`);
-		return result;
-	};
+  return async (...args: unknown[]) => {
+    console.time(`Function Call [${fn.name}]`);
+    const result = await fn(...args);
+    console.timeEnd(`Function Call [${fn.name}]`);
+    return result;
+  };
 }
 
 export interface Action<T extends Context> {
-	get name(): string;
-	handler: (ctx: T) => Promise<unknown>;
+  get name(): string;
+  handler: (ctx: T) => Promise<unknown>;
 }
 
 export abstract class BaseAction<T extends Context> implements Action<T> {
-	abstract get name(): string;
-	abstract get awaitField(): string | undefined;
-	abstract fn(ctx: T): Promise<unknown>;
+  abstract get name(): string;
+  abstract get awaitField(): string | undefined;
+  abstract fn(ctx: T): Promise<unknown>;
 
-	get awaitChange(): string | undefined {
-		return undefined;
-	}
+  get awaitChange(): string | undefined {
+    return undefined;
+  }
 
-	async handler(ctx: T): Promise<unknown> {
-		const startAt = Date.now();
-		console.time(`Action [${this.name}]`);
+  async handler(ctx: T): Promise<unknown> {
+    const startAt = Date.now();
+    console.time(`Action [${this.name}]`);
 
-		const record = { waiting: 0, completed: 0 };
+    const record = { waiting: 0, completed: 0 };
 
-		// Wait for the awaitField to be non-undefined
-		if (this.awaitField) {
-			const awaitField = this.awaitField;
-			const maxTimeout = 60000;
-			const delta = 50;
-			let timeout = 0;
-			while (typeof ctx[awaitField] === "undefined") {
-				if (timeout >= maxTimeout) {
-					break;
-				}
-				await new Promise((resolve) => setTimeout(resolve, delta));
-				timeout += delta;
-			}
-			record.waiting = timeout;
-		}
+    // Wait for the awaitField to be non-undefined
+    if (this.awaitField) {
+      const awaitField = this.awaitField;
+      const maxTimeout = 60000;
+      const delta = 50;
+      let timeout = 0;
+      while (typeof ctx[awaitField] === "undefined") {
+        if (timeout >= maxTimeout) {
+          break;
+        }
+        await new Promise((resolve) => setTimeout(resolve, delta));
+        timeout += delta;
+      }
+      record.waiting = timeout;
+    }
 
-		let result = await this.fn(ctx);
-		if (this.awaitChange) {
-			const oldValue = ctx[this.awaitChange];
-			while (result === oldValue) {
-				await new Promise((resolve) => setTimeout(resolve, 50));
-				result = await this.fn(ctx);
-			}
-		}
+    let result = await this.fn(ctx);
+    if (this.awaitChange) {
+      const oldValue = ctx[this.awaitChange];
+      while (result === oldValue) {
+        await new Promise((resolve) => setTimeout(resolve, 50));
+        result = await this.fn(ctx);
+      }
+    }
 
-		record.completed = Date.now() - startAt;
-		console.timeEnd(`Action [${this.name}]`);
+    record.completed = Date.now() - startAt;
+    console.timeEnd(`Action [${this.name}]`);
 
-		ctx.latencies[this.name] = record;
-		return result;
-	}
+    ctx.latencies[this.name] = record;
+    return result;
+  }
 }
 
 export class Batch<T extends Context> {
-	private _actions: Action<T>[] = [];
-	private _context: T;
+  private _actions: Action<T>[] = [];
+  private _context: T;
 
-	constructor(context: T, actions: Action<T>[]) {
-		this._context = Object.assign({}, context);
-		this._actions = actions;
-	}
+  constructor(context: T, actions: Action<T>[]) {
+    this._context = Object.assign({}, context);
+    this._actions = actions;
+  }
 
-	get context(): T {
-		return this._context;
-	}
+  get context(): T {
+    return this._context;
+  }
 
-	async run(): Promise<void> {
-		// Run all the action parallelly
-		await Promise.all(
-			this._actions.map((action) => action.handler(this._context)),
-		);
-	}
+  async run(): Promise<void> {
+    // Run all the action parallelly
+    await Promise.all(
+      this._actions.map((action) => action.handler(this._context)),
+    );
+  }
 
-	printLatencies() {
-		// Print the latencies in the console with a nice format
-		console.log("\n ---- Latencies ----");
-		for (const [action, record] of Object.entries(this._context.latencies)) {
-			console.log(
-				`- ${action}: ${record.waiting}ms(waiting) - ${record.completed}ms(completed)`,
-			);
-		}
-	}
+  printLatencies() {
+    // Print the latencies in the console with a nice format
+    console.log("\n ---- Latencies ----");
+    for (const [action, record] of Object.entries(this._context.latencies)) {
+      console.log(
+        `- ${action}: ${record.waiting}ms(waiting) - ${record.completed}ms(completed)`,
+      );
+    }
+  }
 }
 
 export async function buildEVMBlockchainContext(privKey: string) {
-	// Create a private key from the environment variable
-	const key = privKey.startsWith("0x") ? privKey.substring(2) : privKey;
+  // Create a private key from the environment variable
+  const key = privKey.startsWith("0x") ? privKey.substring(2) : privKey;
 
-	const account = privateKeyToAccount(`0x${key}`);
-	console.log(`[Address: ${account.address} @${networkName}]`);
+  const account = privateKeyToAccount(`0x${key}`);
+  console.log(`[Address: ${account.address} @${networkName}]`);
 
-	return { account, latencies: {} } as EVMBlockchainContext;
+  return { account, latencies: {} } as EVMBlockchainContext;
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,42 +1,42 @@
 import type {
-	PrivateKeyAccount,
-	WaitForTransactionReceiptReturnType,
+  PrivateKeyAccount,
+  WaitForTransactionReceiptReturnType,
 } from "viem";
 import type { ArgsFn } from "@onflow/fcl-core/types/exec/args";
 import type { Account } from "@onflow/typedefs";
 
 export interface Context extends Record<string, unknown> {
-	latencies: Record<string, Record<"waiting" | "completed", number>>;
+  latencies: Record<string, Record<"waiting" | "completed", number>>;
 }
 
 export type EVMBlockchainContext = {
-	account: PrivateKeyAccount;
-	hash?: string;
-	receipt?: WaitForTransactionReceiptReturnType;
+  account: PrivateKeyAccount;
+  hash?: string;
+  receipt?: WaitForTransactionReceiptReturnType;
 } & Context;
 
 export type Authz = (account: Account) => Promise<object> | object;
 
 export interface IFlowScriptExecutor {
-	/**
-	 * Execute a script
-	 * @param code Cadence code
-	 * @param args Cadence arguments
-	 */
-	executeScript<T>(code: string, args: ArgsFn, defaultValue: T): Promise<T>;
+  /**
+   * Execute a script
+   * @param code Cadence code
+   * @param args Cadence arguments
+   */
+  executeScript<T>(code: string, args: ArgsFn, defaultValue: T): Promise<T>;
 }
 
 /**
  * Signer interface
  */
 export interface IFlowSigner {
-		/**
-		 * Send a transaction
-		 */
-		sendTransaction(code: string, args: ArgsFn, authz?: Authz): Promise<string>;
+  /**
+   * Send a transaction
+   */
+  sendTransaction(code: string, args: ArgsFn, authz?: Authz): Promise<string>;
 
-		/**
-		 * Build authorization
-		 */
-		buildAuthorization(accountIndex?: number, privateKey?: string): Authz;
-	}
+  /**
+   * Build authorization
+   */
+  buildAuthorization(accountIndex?: number, privateKey?: string): Authz;
+}
